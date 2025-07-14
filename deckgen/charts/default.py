ad7702616ee6d65f55933274917aed3b9a93b33e
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2025-07-14 12:18:54 -0500

    Update default.py

--- deckgen/charts/default.py
+++ deckgen/charts/default.py
@@ -34,8 +34,8 @@ class DefaultChart:
             if self.chartType:
                 plt.title(f'{self.chartType.capitalize()} plot of {self.measure} by {self.dimension}')
             # plt.tight_layout(pad=3.0)
-            plt.gcf().set_size_inches(2, 2)
-            plt.savefig(tmpfile.name, bbox_inches='tight',pad_inches= 1.5)
+            plt.gcf().set_size_inches(4, 2)
+            plt.savefig(tmpfile.name) #, bbox_inches='tight',pad_inches= 1.5)
             plt.close()
             image_path = tmpfile.name
         return image_path
