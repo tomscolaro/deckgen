9d5c9241596ddb11bbeddd7fb41f77e89d32cf0a
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2025-07-14 12:38:51 -0500

    Update doublecluster.py

--- deckgen/custom_charts/doublecluster.py
+++ deckgen/custom_charts/doublecluster.py
@@ -81,11 +81,12 @@ class doubleCluster(DefaultChart):
         ax.set_xticks(x,)
         ax.set_xticklabels(all_dates, fontsize=4)
         ax.tick_params(axis='x', labelrotation=45, size=4) 
+        ax.tick_params(axis='y', size=4) 
         ax.set_ylabel('Value', fontsize=4)
         ax.set_title('Clustered Column Chart: Stacked + Total Bars', fontsize=4)
 
         # Move legend to the bottom
-        ax.legend(loc='lower center', bbox_to_anchor=(0.5, -1), fontsize='x-small',ncol=4, frameon=False)
+        ax.legend(loc='lower center', bbox_to_anchor=(1, 0), fontsize='x-small',ncol=4, frameon=False)
 
         img_path = self.save_file()
 
